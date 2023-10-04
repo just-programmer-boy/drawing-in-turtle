e40d181411b38781c18e16fa24d03669aacb5f8b
just-programmer-boy <144595703+just-programmer-boy@users.noreply.github.com>
2023-10-04 16:37:39 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
-#first of all you should install sketchpy in your vs by typing
-#pip install sketchpy
+# first of all you should install sketchpy in your vs code terminal by typing
+# pip install sketchpy
 
-#codes to import the module
+# these are codes to import the module
 from sketchpy import library as lib
 
-#create an object of rdj()
+# codes create an object of rdj()
 obj = lib.rdj()
 
 # you can change speed using pen.speed()
 obj.pen.speed(2)
 
-#the draw method
+# and the draw method
 obj.draw() 
